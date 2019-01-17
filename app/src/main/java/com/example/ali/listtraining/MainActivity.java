6bf3d7ff2071a9489fece8ee38b0bd9272c2dbd8
Ali Aboutalebi <aboutalebi@live.com>
2019-01-17 10:16:50 +0330

    MainActivity: added 2 comment lines as test activityh_main: added a button

--- app/src/main/java/com/example/ali/listtraining/MainActivity.java
+++ app/src/main/java/com/example/ali/listtraining/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.ali.listtraining;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +8,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // changed
+        // git test
     }
 }
